ac9c92442250d01c38424f887ffbfe1a913ede6e
Alisson Zampietro <alisson.zampietro@gmail.com>
2020-02-24 09:09:29 +0000

    feat: add storage

--- lib/modules/TypesWidgets/Statefull.dart
+++ lib/modules/TypesWidgets/Statefull.dart
@@ -8,16 +8,52 @@ class Statefull extends StatefulWidget {
   * */
   @override
   /*
-  * createState, is the method that flutter uses
-  * to create the state method
+  * Creates the mutable state for this widget
+  * at a given location in the tree.
   * */
   _StatefullState createState() => _StatefullState();
 }
 
 class _StatefullState extends State<Statefull> {
+  var _title = 'Instagram';
+  final NameText = TextEditingController();
+  var _text = 'Alisson Zampietro';
+  // obtain shared preferences
+  final prefs = await SharedPreferences.getInstance();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+        backgroundColor: Colors.amber,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              style: new TextStyle(),
+              controller: NameText,
+              decoration: InputDecoration(
+                  labelText: 'Enter your username',
+              ),
+            ),
+            RaisedButton(
+                child: Text('Press here'),
+                color: Colors.deepOrangeAccent,
+                textColor: Colors.white,
+                highlightColor: Colors.amber,
+                onPressed: (){
+                  setState(() {
+                    _text = 'Rodrigues Alisson';
+                  });
+                }
+            ),
+            Text("Nome: ${NameText.value.text}")
+          ],
+        ),
+      ),
+    );
   }
 }
 
