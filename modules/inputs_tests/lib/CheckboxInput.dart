ee8666c782c7fc45a1116b823d3e789584290df4
Alisson Zampietro <alisson.zampietro@gmail.com>
2020-03-06 01:20:21 +0000

    feat: create checkbox

--- modules/inputs_tests/lib/CheckboxInput.dart
+++ modules/inputs_tests/lib/CheckboxInput.dart
@@ -6,12 +6,28 @@ class CheckboxInput extends StatefulWidget {
 }
 
 class _CheckboxInputState extends State<CheckboxInput> {
+
+  bool _likeBrazilianFood = false;
+
+  void toogle(bool value) {
+    setState(() {
+      this._likeBrazilianFood = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text('Funcionando')
-      ],
+    return Padding(
+        padding: EdgeInsets.all(40),
+        child: Row(
+          children: <Widget>[
+            Text('Do you like Brazilian Food?'),
+            Checkbox(
+              value: this._likeBrazilianFood,
+              onChanged: (bool value) => this.toogle(value),
+            )
+          ],
+        ),
     );
   }
 }
